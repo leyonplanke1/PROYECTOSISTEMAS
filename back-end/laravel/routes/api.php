a9c9e2617781339986d297fdc63caa64083f69fa
leyonplanke1 <i2623180@continental.edu.pe>
2022-03-06 23:22:25 -0500

    updtae

--- back-end/laravel/routes/api.php
+++ back-end/laravel/routes/api.php
@@ -4,6 +4,11 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\v1\ProductosController;
 use App\Http\Controllers\v1\CategoriasController;
+use App\User;
+
+use App\Http\Controllers\v2\SeguridadController;
+
+
 
 /*
 |--------------------------------------------------------------------------
@@ -21,17 +26,31 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 
-Route::get("/v1/productos",[ProductosController::class,"getAll"]);
-Route::get("/v1/productos/{id}",[ProductosController::class,"getItem"]);
-Route::post("/v1/productos",[ProductosController::class,"store"]);
-Route::put("/v1/productos",[ProductosController::class,"update"]);
-Route::patch("/v1/productos",[ProductosController::class,"patch"]);
-Route::delete("/v1/productos/{id}",[ProductosController::class,"delete"]);
 
 
-Route::get("/v1/categorias",[CategoriasController::class,"getAll"]);
-Route::get("/v1/categorias/{id}",[CategoriasController::class,"getItem"]);
-Route::post("/v1/categorias",[CategoriasController::class,"store"]);
-Route::put("/v1/categorias",[CategoriasController::class,"update"]);
-Route::patch("/v1/categorias",[CategoriasController::class,"patch"]);
-Route::delete("/v1/categorias/{id}",[CategoriasController::class,"delete"]);
+    Route::get("/v1/productos",[ProductosController::class,"getAll"]);
+    Route::get("/v1/productos/{id}",[ProductosController::class,"getItem"]);
+    Route::post("/v1/productos",[ProductosController::class,"store"]);
+    Route::put("/v1/productos",[ProductosController::class,"update"]);
+    Route::patch("/v1/productos",[ProductosController::class,"patch"]);
+    Route::delete("/v1/productos/{id}",[ProductosController::class,"delete"]);
+
+    Route::get("/v1/categorias",[CategoriasController::class,"getAll"]);
+    Route::get("/v1/categorias/{id}",[CategoriasController::class,"getItem"]);
+    Route::post("/v1/categorias",[CategoriasController::class,"store"]);
+    Route::put("/v1/categorias",[CategoriasController::class,"update"]);
+    Route::patch("/v1/categorias",[CategoriasController::class,"patch"]);
+    Route::delete("/v1/categorias/{id}",[CategoriasController::class,"delete"]);
+
+    Route::get("/v1/seguridad/usuarios",[SeguridadController::class,"getAll"]);
+    Route::get("/v1/seguridad/usuarios/{id}",[SeguridadController::class,"getItem"]);
+    Route::post("/v1/seguridad/usuarios",[SeguridadController::class,"store"]);
+    Route::put("/v1/seguridad/usuarios",[CategoriasController::class,"update"]);
+    Route::patch("/v1/seguridad/usuarios",[CategoriasController::class,"patch"]);
+    Route::delete("/v1/seguridad/usuarios/{id}",[CategoriasController::class,"delete"]);    
+
+
+
+Route::post("/v1/seguridad/login",[SeguridadController::class,"login"]);
+
+
